f2867fbfdc8a6b5a83d67eee16a0c81616ebe3e0
wyclif-maitho <isaacmaitho14@gmail.com>
2023-05-30 13:44:37 +0300

    Added User Routes and Controllers

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const jobRoutes = require('./routes/jobsRoutes')
+const userRoutes = require('./routes/user')
 
 //express app
 const app = express()
@@ -16,6 +17,7 @@ app.use((req, res, next) =>{
 
 //routes
 app.use('/api/jobs',jobRoutes)
+app.use('/api/user',userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
